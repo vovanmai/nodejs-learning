5bc10cd8178a70feb28dd5c09c10b5805f0c6711
Vo Van Mai <mai.vv@neo-lab.vn>
2023-02-18 15:13:52 +0700

    build core

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const express = require("express");
+const cors = require("cors");
 var dotenv = require('dotenv');
 dotenv.config();
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+
+db.sequelize.sync()
+    .then(() => {
+        console.log("Synced db.");
+    })
+    .catch((err) => {
+        console.log("Failed to sync db: " + err.message);
+    });
+
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
 });
 
-io.on('connection', (socket) => {
-    console.log(socket.id)
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-});
-server.listen(process.env.PORT || '3000', () => {
-    console.log('http://localhost:' + process.env.PORT || '3000');
+require("./routes/tutorial.route")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
---
+++ models/tutorial.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, Sequelize) => {
+    const Tutorial = sequelize.define("tutorial", {
+        title: {
+            type: Sequelize.STRING
+        },
+        description: {
+            type: Sequelize.STRING
+        },
+        published: {
+            type: Sequelize.BOOLEAN
+        },
+        active: {
+            type: Sequelize.BOOLEAN
+        }
+    });
+
+    return Tutorial;
+};
